836a0bd16d251837fef106c7e03e95ee8fba23f3
angel <angel.rodriguez3rm@gmail.com>
2025-03-29 19:56:08 +0100

    trip expenses skeleton

--- app/(features)/trips/[id]/expenses/page.tsx
+++ app/(features)/trips/[id]/expenses/page.tsx
@@ -1,5 +1,6 @@
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
+import NewExpenseButton from "../_components/new-expense-creator";
 
 export default async function ProtectedPage() {
   const supabase = await createClient();
@@ -13,8 +14,27 @@ export default async function ProtectedPage() {
   }
 
   return (
-    <div className="flex-1 w-full flex flex-col gap-12">
-      this will be the trip expenses
+    <div className="flex-1 w-full flex flex-col gap-12 justify-start">
+      <div>
+        <NewExpenseButton />
+      </div>
+      <div className="flex flex-row gap-8">
+        <div className="flex-auto justify-start border rounded-lg p-8">
+          this will be the TOTAL spendings
+        </div>
+        <div className="flex-auto justify-start border rounded-lg p-8">
+          this will be spendings/CATEGORY
+        </div>
+      </div>
+      <div>
+        list of spendings in a table with a pagination <br />
+        ---- <br />
+        spending 1 <br />
+        ---- <br />
+        spending 2 <br />
+        ---- <br />
+        ....
+      </div>
     </div>
   );
 }
