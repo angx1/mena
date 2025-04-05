58b909b973765ac265f33dd096090093916e35f7
angel <angel.rodriguez3rm@gmail.com>
2025-04-05 20:17:02 +0200

    Revert "fallo de codigo"
    This reverts commit 5cd69e8eeca234f695a08a9b7a4f5433be7127e8.

--- app/(features)/trips/[id]/notes/page.tsx
+++ app/(features)/trips/[id]/notes/page.tsx
@@ -1,5 +1,6 @@
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
+import NoteEditor from "../_components/novel-editor";
 
 export default async function ProtectedPage() {
   const supabase = await createClient();
@@ -12,5 +13,9 @@ export default async function ProtectedPage() {
     return redirect("/sign-in");
   }
 
-  return <div className="flex-1 w-full flex flex-col gap-12"></div>;
+  return (
+    <div className="flex-1 w-full flex flex-col gap-12">
+      <NoteEditor />
+    </div>
+  );
 }
