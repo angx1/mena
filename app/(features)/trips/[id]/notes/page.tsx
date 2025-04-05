5cd69e8eeca234f695a08a9b7a4f5433be7127e8
angel <angel.rodriguez3rm@gmail.com>
2025-04-05 20:16:56 +0200

    fallo de codigo

--- app/(features)/trips/[id]/notes/page.tsx
+++ app/(features)/trips/[id]/notes/page.tsx
@@ -1,6 +1,5 @@
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
-import NoteEditor from "../_components/novel-editor";
 
 export default async function ProtectedPage() {
   const supabase = await createClient();
@@ -13,9 +12,5 @@ export default async function ProtectedPage() {
     return redirect("/sign-in");
   }
 
-  return (
-    <div className="flex-1 w-full flex flex-col gap-12">
-      <NoteEditor />
-    </div>
-  );
+  return <div className="flex-1 w-full flex flex-col gap-12"></div>;
 }
