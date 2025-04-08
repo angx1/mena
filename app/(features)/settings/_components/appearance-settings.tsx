24f5831c66b022988fa3024df2ebee6b19447551
angel <angel.rodriguez3rm@gmail.com>
2025-04-08 15:06:50 +0200

    appearance settings implemented

--- app/(features)/settings/_components/appearance-settings.tsx
+++ app/(features)/settings/_components/appearance-settings.tsx
@@ -1,7 +1,9 @@
+import { ModeToggle } from "./theme-switcher";
+
 export default function AppearanceSettings() {
   return (
     <div className="border rounded-lg p-8">
-      <h1>appearance Settings</h1>
+      <ModeToggle />
     </div>
   );
 }
